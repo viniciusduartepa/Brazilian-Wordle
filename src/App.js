5d4a4c3843a311c9c32cb4618b9d190ca410692a
Vinicius Alves <viniciusdpalves@gmail.com>
2023-11-30 18:43:24 -0300

    refactor: initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  onChange = (input) => {
+    console.log("Input changed", input);
+  }
+
+  onKeyPress = (button) => {
+    console.log("Button pressed", button);
+  }
+
+  render(){
+    return (
+      <Keyboard
+        onChange={this.onChange}
+        onKeyPress={this.onKeyPress}
+      />
+    );
+  }
 }
 
 export default App;
