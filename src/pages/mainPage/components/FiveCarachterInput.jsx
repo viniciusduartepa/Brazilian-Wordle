6d56c29f816a56289c3bf469187c56a0624e634d
Vinicius Alves <viniciusdpalves@gmail.com>
2023-12-03 11:16:27 -0300

    feat: add inputStatus

--- src/pages/mainPage/components/FiveCarachterInput.jsx
+++ src/pages/mainPage/components/FiveCarachterInput.jsx
@@ -1,42 +1,48 @@
-import React, { useState, useRef } from 'react';
+import React, {useRef, useEffect } from 'react';
 
 export const FiveCharacterInput = (props) => {
-  const [inputValues, setInputValues] = useState(['', '', '', '', '']);
-  
-  const inputRefs = useRef([]);
+
+  const inputRefs = useRef(props.values.map(() => React.createRef()));
 
   const handleInputChange = (index, value) => {
-    // Remover qualquer caractere que não seja uma letra em caixa alta
     const sanitizedValue = value.replace(/[^A-Za-z]/g, '').toUpperCase();
+    props.updateInputValue(index,sanitizedValue);
 
-    const newInputValues = [...inputValues];
-    newInputValues[index] = sanitizedValue;
+    // Move focus to the next input field
+    const nextIndex = (index + 1) % inputRefs.current.length;
+    inputRefs.current[nextIndex].current.focus();
 
-    setInputValues(newInputValues);
-
-    // Mover o foco para o próximo  onInputChange={(value) => handleInputChange(index, value)}nput
-    if (index < inputRefs.current.length - 1 && sanitizedValue !== '') {
-      inputRefs.current[index + 1].focus();
-    }
-    props.onInputChange(newInputValues.join(''));
-
+    // Update the current column index
+    props.setCurrentColumnIndex(nextIndex);
   };
+
+  const handleInputFocus = (index) => {
+    // Update the current column index when focusing on an input
+    props.setCurrentColumnIndex(index);
+  };
+
+  useEffect(() => {
+    // Focus on the initial input field
+    inputRefs.current[0].current.focus();
+  }, []);
 
   return (
     <div>
-      {inputValues.map((value, index) => (
+      {props.values.map((value, index) => (
         <input
           key={index}
           type="text"
           maxLength="1"
           value={value}
           onChange={(e) => handleInputChange(index, e.target.value)}
-          ref={(input) => (inputRefs.current[index] = input)}
+          onFocus={() => handleInputFocus(index)}
           onInput={(e) => {
             // Permitir apenas letras
           e.target.value = e.target.value.replace(/[^A-Za-z]/g, '');
           }}
           disabled={!props.isFocused}
+          ref={inputRefs.current[index]}
+
         />
       ))}
     </div>
--- src/pages/mainPage/components/Game.jsx
+++ src/pages/mainPage/components/Game.jsx
@@ -1,17 +1,97 @@
-import React from 'react';
-import { CustomKeyboard } from './Keyboard';
-
+import React, { useState } from "react";
+import { CustomKeyboard } from "./Keyboard";
+import { FiveCharacterInput } from "./FiveCarachterInput";
+import { validWord } from "../../../services/WordsService";
 
 export const Game = () => {
-    
+  const initialInputValues = Array(6)
+    .fill(["", "", "", "", ""])
+    .map(() => [...Array(5).fill("")]);
+  const [inputValues, setInputValues] = useState(initialInputValues);
+
+  const initialInputStatus = Array(6)
+    .fill(["", "", "", "", ""])
+    .map(() => [...Array(5).fill("")]);
+  const [inputStatus, setInputStatus] = useState(initialInputStatus);
+
+  const [currentRowIndex, setCurrentRowIndex] = useState(0);
+  const [currentColumnIndex, setCurrentColumnIndex] = useState(0);
+
+  const updateInputValue = (rowIndex, columnIndex, newValue) => {
+    const newArray = inputValues.map((row, index) =>
+      index === rowIndex
+        ? [
+            ...row.slice(0, columnIndex),
+            newValue,
+            ...row.slice(columnIndex + 1),
+          ]
+        : row
+    );
+    setInputValues(newArray);
+  };
+
+  const updateInputStatus = (rowIndex, columnIndex, newValue) => {
+    setInputStatus((prevInputStatus) => {
+      const newArray = [...prevInputStatus];
+      newArray[rowIndex] = [
+        ...newArray[rowIndex].slice(0, columnIndex),
+        newValue,
+        ...newArray[rowIndex].slice(columnIndex + 1),
+      ];
+      return newArray;
+    });
+  };
+  
+  const changeRow = async (newRowIndex) => {
+    try {
+      const apiResponse = await validWord(
+        inputValues[currentRowIndex].join("")
+      );
+      if (apiResponse.results) {
+        const resultsArray = Object.values(apiResponse.results);
+
+        await Promise.all(
+          resultsArray.map((value, index) =>
+            updateInputStatus(currentRowIndex, index, value)
+          )
+        );
+      }
+    } catch (error) {
+      console.error("Erro ao obter dados válidos:", error);
+    }
+    setCurrentRowIndex(newRowIndex);
+  };
+
   return (
     <div>
-        <CustomKeyboard
-        missplacedChars={["A","B","C"]}
-        correctChars={["D","E","F"]}
-        wrongChars={["G","H","I"]}
+      {inputValues.map((values, index) => (
+        <FiveCharacterInput
+          key={index}
+          isFocused={index === currentRowIndex}
+          values={values}
+          updateInputValue={(columnIndex, value) =>
+            updateInputValue(currentRowIndex, columnIndex, value)
+          }
+          currentColumnIndex={currentColumnIndex}
+          setCurrentColumnIndex={setCurrentColumnIndex}
         />
+      ))}
+      <button onClick={() => changeRow((currentRowIndex + 1) % 6)}>
+        Change Row
+      </button>
+      <CustomKeyboard
+        missplacedChars={["A", "B", "C"]}
+        correctChars={["D", "E", "F"]}
+        wrongChars={["G", "H", "I"]}  
+      />
+      {inputStatus.map((row, rowIndex) => (
+  <div key={rowIndex}>
+    {row.map((value, columnIndex) => (
+      <p key={columnIndex}>{Array.isArray(value) ? value.join(', ') : value}</p>
+    ))}
+  </div>
+))}
+
     </div>
   );
 };
-
